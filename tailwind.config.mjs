8a3964302893dcc3e130d9dc771055b52585f4f1
JaviiAzagra <javiazagra12@gmail.com>
2024-10-19 18:30:26 +0200

    Refactor image file formats and update layout in Hero and Layout components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
         font: "#FFFFFF",
         font2: "#B6C3D0",
-        bgcolor: "#000000",
+        bgcolor: "#000306",
         borderColor: "#9A9A9A",
         orange: "#FF6D00",
       },
