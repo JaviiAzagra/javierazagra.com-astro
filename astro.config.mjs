941e6a6a18678f43ad651a4956d052b8691ed6c7
JaviiAzagra <javiazagra12@gmail.com>
2024-10-21 00:38:15 +0200

    Refactor package.json and astro.config.mjs
    Update dependencies in package.json to include @astrojs/vercel.
    Add vercel adapter and enable web analytics in astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "de"],
@@ -13,4 +15,9 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
